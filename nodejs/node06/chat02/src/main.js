6d785bc695f67015eee4bb93fb0b4cb7595cd202
SooBookE <thksl229@gmail.com>
2023-01-05 12:58:01 +0900

    대대적인 수정

--- nodejs/node06/chat02/src/main.js
+++ nodejs/node06/chat02/src/main.js
@@ -4,4 +4,10 @@ import router from './router'
 import store from './store'
 import { io } from 'socket.io-client'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+const socket = io()
+
+app.config.globalProperties.$socket = socket
+app.config.globalProperties.kys = '짱멋짐'
+
+app.use(store).use(router).mount('#app')
